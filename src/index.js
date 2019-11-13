aae696e6be3978a022d61d6653ed5ca07564b896
Damien Christophe <damien.christophe@epsor.fr>
2019-11-13 17:21:47 +0100

    fix: 🐛 Delete default port 8000

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import logger from '@epsor/logger';
 
 import app from './app';
 
-const PORT = process.env.HEALTHCHECK_PORT || 8000;
+const PORT = process.env.HEALTHCHECK_PORT;
 
 (async () => {
+  if (!PORT) {
+    logger.info(`consumer port is not defined. HEALTHCHECK disabled`, { tags: ['consumer'] });
+    return;
+  }
   await app.listen(PORT, () => {
     logger.info(`consumer-wrapper listening`, { port: PORT, tags: ['consumer', 'health-check'] });
   });
