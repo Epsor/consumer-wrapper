5e3b1d49ced03f3dbe0e40b42d962b1d8f21900b
François Guillot <francois.guillot@epsor.fr>
2019-11-14 17:57:20 +0100

    feat: 🎸 Remove express healthcheck app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import logger from '@epsor/logger';
-
-import app from './app';
-
-const PORT = process.env.HEALTHCHECK_PORT;
-
-(async () => {
-  if (!PORT) {
-    logger.info(`consumer port is not defined. HEALTHCHECK disabled`, { tags: ['consumer'] });
-    return;
-  }
-  await app.listen(PORT, () => {
-    logger.info(`consumer-wrapper listening`, { port: PORT, tags: ['consumer', 'health-check'] });
-  });
-})();
-
 export { default as AbstractHandler } from './abstractHandler';
 export { default as Consumer } from './consumer';
 export { default as HandlerError } from './handlerError';
